5c481b0c04a7df8e489bc71c238ac31596b43acc
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-04-12 13:40:14 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://pixabay.com';
 
 export const getData = async (searchQuery, currentPage) => {
-  const response = await axios.get('/api', {
+  const response = await axios.get('https://pixabay.com/api', {
     params: {
       key: '43212506-95870309335e8ebf3ea9c8656',
       q: searchQuery,
